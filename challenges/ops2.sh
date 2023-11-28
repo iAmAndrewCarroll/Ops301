4f9152d1d9c242378f67d661bcba04f89bfe7775
iAmAndrewCarroll <athomascarroll@gmail.com>
2023-11-28 12:47:24 -0800

    challenge 2 script

--- challenges/ops2.sh
+++ challenges/ops2.sh
@@ -2,3 +2,10 @@
 
 # defining source and destination filenames
 src_file="/var/log/syslog"
+current=$(date +"%Y-%m-%d-%H:%M:%S")
+dest_file="/home/sysadmin/backups/syslog-$current"
+
+# copy syslog file to current working directory with timestamp
+cp $src_file $dest_file
+
+echo "Log file copied to: $dest_file"
