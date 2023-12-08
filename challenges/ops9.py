d149e19b1dddaf0aa3072835afd5db304e646afa
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-12-07 17:28:16 -0800

    ops9

---
+++ challenges/ops9.py
@@ -0,0 +1,39 @@
+# Assign values to variables a and b
+a = 10
+b = 5
+
+# Check if a is equal to b
+if a == b:
+    print("a is equal to b")
+
+# Check if a is not equal to b
+elif a != b:
+    print("a is not equal to b")
+
+# Check if a is greater than b
+if a > b:
+    print("a is greater than b")
+
+# Check if a is less than or equal to b
+elif a <= b:
+    print("a is less than or equal to b")
+
+# Check if both conditions are met using 'and'
+if a > 5 and b < 7:
+    print("Both conditions are met (a > 5 and b < 7)")
+
+# Check if either condition is met using 'or'
+if a > 15 or b < 3:
+    print("Either condition is met (a > 15 or b < 3)")
+
+# Nested if statement
+if a > 8:
+    print("a is greater than 8")
+    if b < 3:
+        print("b is less than 3")
+
+# Create an if statement with pass
+if a < 3:
+    pass
+else:
+    print("a is not less than 3")
